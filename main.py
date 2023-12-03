a9da9357b77d8772f613d008477b3061a8ed115a
anonymous123-acesta <64437792+anonymous123-acesta@users.noreply.github.com>
2023-12-03 14:48:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ def main():
 
     t = transaction_true(account)
     t1 = transaction_false_amount(account)
-    t2 = transaction_pending(account)
-    t3 = transaction2_true(account)
-    t4 = transaction_EGDT_true()
-    t5 = transaction_EGDT_false()
-    t6 = transaction_smart_contract()
+    t2 = transaction2_true(account)
+    t3 = transaction_EGDT_true()
+    t4 = transaction_EGDT_false()
+    t5 = transaction_smart_contract()
 
     t.nonce = nonce_holder.get_nonce_then_increment()
     t1.nonce = nonce_holder.get_nonce_then_increment()
@@ -23,17 +22,15 @@ def main():
     t3.nonce = nonce_holder.get_nonce_then_increment()
     t4.nonce = nonce_holder.get_nonce_then_increment()
     t5.nonce = nonce_holder.get_nonce_then_increment()
-    t6.nonce = nonce_holder.get_nonce_then_increment()
 
-    tx = semnare(t, signer, transc, proxy)
-    tx1 = semnare(t1, signer, transc, proxy)
-    tx2 = semnare(t2, signer, transc, proxy)
-    tx3 = semnare(t3, signer, transc, proxy)
-    tx4 = semnare(t4, signer, transc, proxy)
-    tx5 = semnare(t5, signer, transc, proxy)
-    tx6 = semnare(t6, signer, transc, proxy)
+    tx = semnare(t, signer, transc)
+    tx1 = semnare(t1, signer, transc)
+    tx2 = semnare(t2, signer, transc)
+    tx3 = semnare(t3, signer, transc)
+    tx4 = semnare(t4, signer, transc)
+    tx5 = semnare(t5, signer, transc)
 
-    hashes = proxy.send_transactions([t, t1, t2, t3, t4, t5, t6])
+    hashes = proxy.send_transactions([t, t1, t2, t3, t4, t5])
     print("Transactions hashes:", hashes)
 
     delay(8)
@@ -44,7 +41,6 @@ def main():
     output(t3, proxy, hashes, '3')
     output(t4, proxy, hashes, '4')
     output(t5, proxy, hashes, '5')
-    output(t6, proxy, hashes, '6')
 
     report_file(t, hashes, '0', proxy)
     report_file(t1, hashes, '1', proxy)
@@ -52,7 +48,6 @@ def main():
     report_file(t3, hashes, '3', proxy)
     report_file(t4, hashes, '4', proxy)
     report_file(t5, hashes, '5', proxy)
-    report_file(t6, hashes, '6', proxy)
 
 if __name__ == '__main__':
     main()
